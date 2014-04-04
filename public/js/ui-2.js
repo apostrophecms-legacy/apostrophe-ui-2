bc4e8b23508e9f06a18808b99c7f294477ee437e
Kerry Gilbert <kerry@punkave.com>
2014-04-04 15:51:09 -0400

    push it good

--- public/js/ui-2.js
+++ public/js/ui-2.js
@@ -1,9 +1,19 @@
 $(function(){
-  $(window).scroll(function(){
-    if($(window).scrollTop() > 100) {
-      $('.apos-admin-bar').addClass('collapsed');
-    } else {
-      $('.apos-admin-bar').removeClass('collapsed');
-    }
+  setTimeout(function(){
+    $(window).on('scroll', function(){
+      $(window).off();
+      setTimeout(function(){
+        $('.apos-admin-bar').addClass('collapsed');
+      }, 1000); 
+    });
+  }, 1000);
+
+  $('.apos-logo-tiny').on('click', function(){ 
+    $('.apos-admin-bar').css('overflow', 'hidden');  
+    $('.apos-admin-bar').toggleClass('collapsed');  
+  });
+
+  $('.apos-admin-bar').on('transitionend', function(){
+    if(!$(this).hasClass('collapsed')) { $(this).css('overflow', 'visible'); }
   });
 });
